b9de6a2bd25ee170409363df08fa08b893ee3b90
Matheus Dussin Bampi <matheusbampi@gmail.com>
2019-09-05 11:00:06 -0300

    DataAPI class finished

--- mbtc/DataAPI.py
+++ mbtc/DataAPI.py
@@ -4,20 +4,53 @@ import requests
 
 class DataAPI:
 
-    METHODS = ["ticker",
-               "orderbook",
-               "trades",       # /?since=<since> OR /<from> OR /<from>/<to>
-               "day-summary"]  # /<year>/<month>/<day>
-
-    URL = 'https://www.mercadobitcoin.net/api/'
-
     @staticmethod
-    def request_data(coin, method, params=None):
-
-        if params is None:
-            url = DataAPI.URL + coin + method
-        else:
-            url = DataAPI.URL + coin + method + params
+    def __request(coin, method, params=None):
+        url = 'https://www.mercadobitcoin.net/api'
+        url += '/' + coin + '/' + method
+        if params is not None:
+            url += '/' + params
 
         response = requests.get(url)
         return response.json()
+
+    @staticmethod
+    def ticker(coin):
+        method = 'ticker'
+        resp = DataAPI.__request(coin, method)
+        return resp
+
+    @staticmethod
+    def orderbook(coin):
+        method = 'orderbook'
+        resp = DataAPI.__request(coin, method)
+        return resp
+
+    @staticmethod
+    def trades(coin, since_id=None, from_timestamp=None, to_timestamp=None):
+        method = 'orderbook'
+        params = None
+
+        # Check parameters
+        if since_id is not None:
+            params = "?since=" + since_id
+        elif from_timestamp is not None:
+            params = from_timestamp
+            if to_timestamp is not None:
+                params += '/' + to_timestamp
+
+        resp = DataAPI.__request(coin, method, params)
+        return resp
+
+    @staticmethod
+    def day_summary(coin, day=None, month=None, year=None):
+        method = "day-summary"
+
+        # Check parameters
+        if None not in [day, month, year]:
+            params = year + "/" + month + "/" + day
+        else:
+            params = None
+
+        resp = DataAPI.__request(coin, method, params)
+        return resp
